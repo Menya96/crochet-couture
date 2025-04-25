d965bdeda7f762914dc4c4be874cd12702d7d087
menyahtonny <menyahtonny@gmail.com>
2025-04-25 08:57:36 +0000

    implimented animejs animation to the h1 in landingpage

--- src/components/landingpage.tsx
+++ src/components/landingpage.tsx
@@ -1,13 +1,29 @@
+import React, { useEffect } from 'react';
+import { createTimeline, stagger } from 'animejs';
 
-import React from 'react';
+const LandingPage: React.FC = () => {+
+  useEffect(() => {
+    //animation that handles the h1 animation
+    const timeline =  createTimeline({ defaults: { duration: 800 } });
+    timeline.label('start')
+      .add('.letter', { y: ['1.1em', 0] ,z: 0, opacity: [0, 1], delay: stagger(50)}, 'start');
+  }, []);
 
-const LandingPage: React.FC = () => {
+  const text = "Unique Threads, Unique You: Find Your Style at Crochet Couture!";
+  const letters = text.split("");
+
   return (
-    <div className="bg-secondaryBrown min-h-screen flex items-center justify-center">
+    <div className="bg-secondaryBrown min-h-screen flex">
       <div className="container flex flex-col md:flex-row items-center justify-center">
-        <div className="md:w-1/2 my-4 text-center flex flex items-center justify-center flex-col bg-blue-300">
-          <h1 className="text-5xl font-bold text-accentGold">Unique Threads, Unique You:Find your style at Crochet Couture!</h1>
-          <p className="mt-4 text-2xl text-accentGold">Wrap yourself in excellence and experience the warmth and comfort of handmade quality crochets.</p>
+        <div className="md:w-1/2 my-4 text-center flex flex-wrap items-center justify-center flex-col">
+          <div className="text-4xl md:text-5xl font-bold text-accentGold ml6">
+            <h1>
+              {letters.map((letter, index) => (
+                <span key={`letter-${index}`} className="letter">{letter}</span>
+              ))}
+            </h1>
+          </div>
+          <p className="mt-10 text-xl text-accentGold">Wrap yourself in excellence and experience the warmth and comfort of handmade quality crochets.</p>
         </div>
         <div className="md:w-1/2 md:mt-0">
           <img alt="hero" className="w-full h-auto rounded-lg" src="/images/pexels-caiquethecreator-25311775.png" />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script type="module" src="https://cdn.jsdelivr.net/npm/animejs@4.0.2/+esm"></script>        
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
