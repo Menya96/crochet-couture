a6b9740dc9720f8902f7c9a3807347abc9199092
menyahtonny <menyahtonny@gmail.com>
2025-05-05 14:24:27 +0000

    scroll animation on collections component done with intersection observer api

--- src/components/Collections.tsx
+++ src/components/Collections.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const categories = [
   { name: "Bags", image: "./images/crochet-bag.webp" },
@@ -10,22 +10,78 @@ const categories = [
   { name: "Sweaters", image: "./images/crochet-sweater.jpg" },
   { name: "Tops", image: "./images/crochet-tops.webp" },
 ];
+const staggerVariants = ['left', 'right'];
+
+
 const Collections: React.FC = () => {
+  const cardRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const headingRef = useRef<HTMLHeadingElement>(null);
+  const paragraphRef = useRef<HTMLParagraphElement>(null);
+
+  useEffect(() => {
+    const cardObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const card = entry.target as HTMLElement;
+        const row = card.dataset.row;
+        if (entry.isIntersecting) {
+          card.classList.add(`animate-${row}`);
+        } else {
+          card.classList.remove(`animate-${row}`);
+        }
+      });
+    }, {
+      threshold: 0.5,
+    });
+    cardRefs.current.forEach((card) => {
+      if (card) {
+        cardObserver.observe(card);
+      }
+    });
+    const headingObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate-heading');
+        }
+      });
+    }, { threshold: 0.5 });
+
+    const paragraphObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate-paragraph');
+        }
+      });
+    }, { threshold: 0.5 });
+
+    if (headingRef.current) {
+      headingObserver.observe(headingRef.current);
+    }
+    if (paragraphRef.current){
+      headingObserver.observe(paragraphRef.current)
+    }
+  }, []);
+
 
   return (
     <div className='bg-white min-h-screen flex w-screen flex-col items-center '>
-      <div className=''>
-        <h1 className='mt-4 md:text-5xl text-2xl font-bold text-center text-primaryGreen font-extrabold '>
+      <div className='flex flex-col justify-center items-center'>
+        <h1 ref={headingRef} className='mt-4 md:text-5xl text-2xl font-bold text-center text-primaryGreen font-extrabold '>
           Unveil Your Unique Style with Crochet
         </h1>
-        <p className="px-2 text-center text-primaryGreen mt-4 md:text-2xl font-bold">
+        <p ref={paragraphRef} className="px-2 text-center text-primaryGreen mt-4 md:text-2xl font-bold">
           From intricate patterns to elegant designs, Explore our handcrafted crochet pieces.
         </p>
       </div>
-      <div className='container grid grid-cols-1 md:grid-cols-4 gap-4 mt-8'>
-        {categories.map(({ name, image }, i) => (
-          <div key={i} className="card bg-gray-100 rounded-lg shadow-md p-4 flex flex-col items-center overflow-hidden hover:translate-y-[-5px] hover:rotate-[-1deg] transition duration-300 ease-in-out">
+      <div className='container grid grid-cols-1 md:grid-cols-4 gap-4 mt-8 px-6 ' >
+        {categories.map(({ name, image }, index) => ( <div 
+          ref={(el) => {
+            if (el) {
+              cardRefs.current[index] = el;
+            }
+          }}
+          key={index} data-row={staggerVariants[index % 2]} className="card bg-gray-100 rounded-lg shadow-md p-4 flex flex-col items-center overflow-hidden hover:translate-y-[-5px] hover:rotate-[-1deg] transition duration-300 ease-in-out">
             <img
+            
               src={image}
               alt={name}
               className="w-full h-full object-cover mb-4"
