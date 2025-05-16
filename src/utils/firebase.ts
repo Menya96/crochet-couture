41e4b889fd398b6d08e5a868396b8dccda5ba7b8
menyahtonny <menyahtonny@gmail.com>
2025-05-16 13:07:49 +0000

    hidden my firebase api keys

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,22 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth"; // Import getAuth
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCPO2VtB4RYZFzohFY9-mMiMahX9Dgdq-w",
-  authDomain: "crochet-couture.firebaseapp.com",
-  projectId: "crochet-couture",
-  storageBucket: "crochet-couture.appspot.com",
-  messagingSenderId: "459783652496",
-  appId: "1:459783652496:web:88dfd71bc9d8e800210415",
-  measurementId: "G-MWHRQKGWJ4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 // Export Auth module
 export const auth = getAuth(app);
